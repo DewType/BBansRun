f33c7a8945afd5c7b9ec0068749824c9303a00be
qoehd <qoehd@DESKTOP-BKHLQLJ>
2019-05-13 16:40:00 +0900

    제발

--- BBansRun/src/MainGame/MainGameUI.java
+++ BBansRun/src/MainGame/MainGameUI.java
@@ -1,10 +1,5 @@
 package MainGame;
 
-<<<<<<< HEAD
-public class MainGameUI{
-	int a = 10;
-=======
 public class MainGameUI {
  private int a = 4444;
->>>>>>> branch 'master' of https://github.com/DewType/BBansRun.git
 }
